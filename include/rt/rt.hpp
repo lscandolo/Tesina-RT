9ee0b0eeab6f880d4001263f8e766f4280764b4c
Leonardo <leo@caladan.(none)>
2012-02-28 12:13:52 -0300

    Added missing rt.hpp

---
+++ include/rt/rt.hpp
@@ -0,0 +1,22 @@
+#ifndef RT_HPP
+#define RT_HPP
+
+#include <rt/math.hpp>
+#include <rt/ray.hpp>
+#include <rt/light.hpp>
+#include <rt/timing.hpp>
+#include <rt/camera.hpp>
+#include <rt/primary-ray-generator.hpp>
+#include <rt/secondary-ray-generator.hpp>
+#include <rt/cubemap.hpp>
+#include <rt/framebuffer.hpp>
+#include <rt/ray-shader.hpp>
+#include <rt/tracer.hpp>
+#include <rt/scene.hpp>
+#include <rt/obj-loader.hpp>
+#include <rt/scene.hpp>
+#include <rt/bvh.hpp>
+#include <rt/cl_aux.hpp>
+
+
+#endif /* RT_HPP */
